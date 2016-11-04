7d06601bea1ec191c71984c64ac281015c3d4b4a
tehlinger <ehlinger.thibaut@gmail.com>
2016-11-04 14:32:01 +0100

    Added homepage and 404 not found.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+   def render_404
+    respond_to do |format|
+      format.html { render template: 'errors/not_found', status: 404 }
+      format.all { render nothing: true, status: 404 }
+    end
+  end
   protect_from_forgery with: :exception
+  #Render 404 if not found
+  rescue_from ActionController::RoutingError, with: -> { render_404  }
+  rescue_from ActionController::UnknownController, with: -> { render_404  }
+  rescue_from ActiveRecord::RecordNotFound, with: -> { render_404  }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "/pages/:page" => "pages#show"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +54,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root "pages#show", page: "home"
+  get '*not_found', to: 'errors#error_404'
 end
