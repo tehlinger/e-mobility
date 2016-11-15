67f20bafd11faa7b0ba196d5f03aae0f5088818f
tehlinger <ehlinger.thibaut@gmail.com>
2016-11-15 12:14:28 +0100

    Link to admin dashboard path and authentication

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -6,7 +6,11 @@ RailsAdmin.config do |config|
   # config.authenticate_with do
   #   warden.authenticate! scope: :user
   # end
-  # config.current_user_method(&:current_user)
+  config.current_user_method(&:current_user)
+  
+  config.authorize_with do
+    redirect_to main_app.root_path unless current_user && current_user.admin?
+  end
 
   ## == Cancan ==
   # config.authorize_with :cancan
